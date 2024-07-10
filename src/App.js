8329c785e414602ed38fcb4585a4cf572fba48dc
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-07-10 13:42:20 +0530

    random working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
-import words from "./vocab.json";
+import wordsData from "./vocab.json";
+
+const words = wordsData.words;
+
+function getRandom() {
+  return Math.floor(Math.random() * words.length);
+}
+
 function App() {
+  const [random, setRandom] = useState(getRandom());
+
+  const handleClick = () => {
+    setRandom(getRandom());
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <div className="word">
-          <h1>{words.word}</h1>
-        </div>
+        <h1>{words[random].word}</h1>
+        <h2>{words[random].definition}</h2>
+        <button onClick={handleClick}>Next Word</button>
       </div>
     </div>
   );

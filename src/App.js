32a9f1ad8d8debb18e5b3926cceecc61dbe4e5bb
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-07-10 14:00:53 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import wordsData from "./vocab.json";
 
 const words = wordsData.words;
 
-function getRandom() {
-  return Math.floor(Math.random() * words.length);
+function getRandom(exclude) {
+  let random;
+  do {
+    random = Math.floor(Math.random() * words.length);
+  } while (exclude.includes(random));
+  return random;
 }
 
 function App() {
-  const [random, setRandom] = useState(getRandom());
+  const [random, setRandom] = useState(getRandom([]));
+  const [viewedWords, setViewedWords] = useState([]);
 
   const handleClick = () => {
-    setRandom(getRandom());
+    const newRandom = getRandom(viewedWords.map((word) => word.index));
+    setViewedWords([...viewedWords, { index: random, ...words[random] }]);
+    setRandom(newRandom);
   };
 
   return (
@@ -21,7 +28,31 @@ function App() {
         <h1>{words[random].word}</h1>
         <h2>{words[random].definition}</h2>
         <button onClick={handleClick}>Next Word</button>
+        <h3>Flashcards Viewed in this session: {viewedWords.length + 1}</h3>
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Sr No</th>
+            <th>Word</th>
+            <th>Definition</th>
+          </tr>
+        </thead>
+        <tbody>
+          {viewedWords.map((word, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{word.word}</td>
+              <td>{word.definition}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <footer>
+        <a href="https://www.vocabulary.com/">
+          Words Obtained from vocabulary.com
+        </a>
+      </footer>
     </div>
   );
 }
